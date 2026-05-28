e4d45ff6b8a37c24c3ebe422e61f60b3389e1b3e
Peyton Strack <peytonstrack@gmail.com>
2026-05-28 16:14:50 -0400

    Updated the script to include 6am to 9am
    94.3 The Shark streams 99.1 PLR from 6 to 9 so, I added a way to get the plr data

---
+++ scripts/Shark Project.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(lubridate)
+library(jsonlite)
+
+url <- "https://us.production.audio.one/api/v1.3/app/20008/playlist/6/live-stream-history/?page_size=100"
+
+plr_url <- "https://us.production.audio.one/api/v1.3/app/20009/playlist/12/live-stream-history/?page_size=100"
+
+#Access the JSON Data
+radio_data <- fromJSON(url)
+
+plr_data <- fromJSON(plr_url)
+
+#Filter the Data and Alter the Format
+radio_data$results %>%
+  select(
+    Artist = current_artist_name,
+    Song = current_title,
+    Time = streamed_time
+  ) %>%
+  mutate(
+    Time = ymd_hms(Time, tz = "UTC"),
+    Time = with_tz(Time, tzone = "America/New_York"),
+    Date = as.Date(Time)
+  ) %>%
+  filter(!(hour(Time) >= 6 & hour(Time) < 9)) -> shark_songs
+
+plr_data$results %>%
+  select(
+    Artist = current_artist_name,
+    Song = current_title,
+    Time = streamed_time
+  ) %>%
+  mutate(
+    Time = ymd_hms(Time, tz = "UTC"),
+    Time = with_tz(Time, tzone = "America/New_York"),
+    Date = as.Date(Time)
+  ) %>%
+  filter(hour(Time) >= 6 & hour(Time) < 9) -> plr_songs
+
+bind_rows(shark_songs, plr_songs) %>%
+  mutate(Time = format(Time,"%I:%M %p")) -> songs
+
+#Ensure the Shark Data exists in the Data file
+if(file.exists("Shark_Data.csv")){
+  
+  read_csv("Shark_Data.csv", show_col_types = FALSE) %>%
+    mutate(Time = as.character(Time), Date = as.Date(Date)) -> old_songs
+  
+} else 
+{
+  old_songs <- tibble()
+}
+
+# Combine old and new data and remove exact duplicates
+bind_rows(old_songs, songs) %>%
+  distinct(Song, Artist, Time, Date, .keep_all = TRUE) %>%
+  arrange(Date, parse_date_time(Time,"%I:%M %p")) -> updated_songs
+
+# Save updated data
+write_csv(updated_songs, "Shark_Data.csv")
+
