2a20698166685e47dfa64d3a7a76f72bb48b1486
Peyton Strack <peytonstrack@gmail.com>
2026-06-09 15:09:19 -0400

    Update shark_data_collection.R
    Fixed another issue with AFI

--- scripts/shark_data_collection.R
+++ scripts/shark_data_collection.R
@@ -11,7 +11,7 @@ shark_data <- fromJSON(url)
 
 plr_data <- fromJSON(plr_url)
 
-artist_fixes <- c("Blink 182" = "Blink-182", "Guns 'n Roses" = "Guns N' Roses", "Ac/dc" = "AC/DC", "P.o.d." = "P.O.D.", "R.e.m." = "R.E.M.", "A.f.i." = "A.F.I.")
+artist_fixes <- c("Blink 182" = "Blink-182", "Guns 'n Roses" = "Guns N' Roses", "Ac/dc" = "AC/DC", "P.o.d." = "P.O.D.", "R.e.m." = "R.E.M.", "A.f.i." = "A.F.I.", "Afi" = "A.F.I.")
 song_fixes <- c("Eruption/you Really Got Me" = "Eruption + You Really Got Me")
 
 #Filter the Data and Alter the Format
