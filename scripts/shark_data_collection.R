4fe7dd28dccf46b5404a539c8436abaed4959573
Peyton Strack <peytonstrack@gmail.com>
2026-06-18 07:52:02 -0400

    Update shark_data_collection.R
    Fixed issue with Dr. Feelgood naming

--- scripts/shark_data_collection.R
+++ scripts/shark_data_collection.R
@@ -12,7 +12,7 @@ shark_data <- fromJSON(url)
 plr_data <- fromJSON(plr_url)
 
 artist_fixes <- c("Blink 182" = "Blink-182", "Guns 'n Roses" = "Guns N' Roses", "Ac/dc" = "AC/DC", "P.o.d." = "P.O.D.", "R.e.m." = "R.E.M.", "A.f.i." = "A.F.I.", "Afi" = "A.F.I.", "The Smashing Pumpkins" = "Smashing Pumpkins")
-song_fixes <- c("Eruption/you Really Got Me" = "Eruption + You Really Got Me", "(you Gotta) Fight For Your Right To Party" = "(You Gotta) Fight For Your Right To Party", "White Wedding (part 1)" = "White Wedding")
+song_fixes <- c("Eruption/you Really Got Me" = "Eruption + You Really Got Me", "(you Gotta) Fight For Your Right To Party" = "(You Gotta) Fight For Your Right To Party", "White Wedding (part 1)" = "White Wedding", "Dr. Feel Good" = "Dr. Feelgood")
 
 #Filter the Data and Alter the Format
 shark_data$results %>%
